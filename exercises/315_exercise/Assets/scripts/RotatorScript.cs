c232c0cc99d06785e3a625d27e8118fef863e490
Stephanie Dang <stphnie.dang@gmail.com>
2022-01-21 12:27:15 -0500

    EX1: Added houses and rotation

---
+++ exercises/315_exercise/Assets/scripts/RotatorScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorScript : MonoBehaviour
+{
+    // variables
+    public float rotationSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    // void Rotate1()
+    // {
+    //     this.GetComponent<Transform>().Rotate(0f,rotationSpeed,0f);
+    // }
+
+    //     void Rotate2()
+    // {
+    //     this.GetComponent<Transform>().Rotate(0f,-rotationSpeed,0f);
+    // }
+}
